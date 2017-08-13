38ea4599900990b5f6a4b6fa3638b95fcee484c6
PayneJoe <yuanpingzhou@sensetime.com>
2017-08-13 19:13:36 +0800

    refactor source code tree

--- clean.sh
+++
@@ -1,7 +0,0 @@
-rm -r CMakeFiles
-rm -r ./index/CMakeFiles
-rm ./index/cmake_install.cmake
-rm -r ./utils/CMakeFiles
-rm ./utils/cmake_install.cmake
-rm -r ./test/CMakeFiles
-rm ./test/cmake_install.cmake
--- cmake.sh
+++
@@ -1,1 +0,0 @@
-cmake -D CMAKE_C_COMPILER=/usr/local/bin/gcc -D CMAKE_CXX_COMPILER=/usr/local/bin/g++ .
